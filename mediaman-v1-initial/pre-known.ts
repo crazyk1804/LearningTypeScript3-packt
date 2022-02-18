97c0051e471b08514c5763d0f3650a1efdc787b3
k <hans1804@daum.net>
2022-02-18 16:44:10 +0900

    ...

--- mediaman-v1-initial/pre-known.ts
+++ mediaman-v1-initial/pre-known.ts
@@ -215,3 +215,134 @@ console.log(isDog({
     race: 'Saint-Hubert',
     nightVision: 12
 }));
+
+
+
+
+
+enum VehicleType {
+    Car, Bus, Train
+}
+
+let myVehicleType = VehicleType.Car;
+console.log('My vehicle type: ', myVehicleType);
+
+
+enum VehicleType {
+    Truck = 1
+}
+
+console.log(
+    'My vehicle type: ', VehicleType.Car,
+    ' New vehicle type: ', VehicleType.Truck
+)
+
+enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+const june: Month = Month.June;
+const nameOfJuneEntry: string = Month[june];
+console.log(nameOfJuneEntry);
+
+
+
+enum TShirtType {
+    CrewNeck = 'Crew Neck',
+    VNeck = 'V Neck',
+    Henley = 'Henley',
+    Polo = 'Polo',
+    SpecialPolo = Polo,
+    ScoopNeck = 'ScoopNeck'
+}
+let myTShirtType = TShirtType.SpecialPolo;
+console.log('My T-Shirt type: ', myTShirtType);
+
+
+
+
+const enum TransientEnum {
+    A, B
+}
+console.log(TransientEnum.A);
+
+
+const enum TransientEnum {
+    C = 3
+}
+
+let te: TransientEnum = TransientEnum.C;
+console.log(TransientEnum['C']);
+
+
+
+
+
+// 어따 쓰는거야 대체..
+function doSomethingForever(): never {
+    while(true) {
+        console.log('Still busy...');
+    }
+}
+
+
+
+
+type Mood = 'Great' | 'Good' | 'Awful';
+const myCurrentMood: Mood = 'Great'; // may only be assigned values that are part of the Mood union
+
+
+
+
+
+interface BoardGame {
+    name: string;
+    description: string;
+    minimalAge: number;
+    players: string;
+    duration: string;
+}
+
+interface VideoGame {
+    name: string;
+    description: string;
+    minimalAge: number;
+    players: string;
+    online: boolean;
+}
+
+function displayGame(game: VideoGame | BoardGame) {
+    console.log(`Game anem: ${ game.name }`);
+}
+
+let boardGame: BoardGame = {
+    name: 'board',
+    description: 'game that can play via board',
+    minimalAge: 100,
+    players: 'womens only',
+    duration: 'night long'
+}
+displayGame(boardGame);
+
+console.log('why this is not printed?');
+
+
+type newType = {
+    a: string;
+    b: string;
+}
+
+function printType(theType: newType) {
+    console.log(theType);
+}
